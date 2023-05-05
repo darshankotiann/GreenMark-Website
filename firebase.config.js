d732d68b49e6f160fa30e21714021756d377e3e2
darshankotiann <darshankotiann@gmail.com>
2023-05-05 19:06:34 +0530

    Done

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+//  const firebaseConfig = {
+//    apiKey: "AIzaSyBEdY3S1jBFEeGNTXCtt72D8zKd6G50yWg",
+//    authDomain: "greenmark-admin.firebaseapp.com",
+//    projectId: "greenmark-admin",
+//    storageBucket: "greenmark-admin.appspot.com",
+//    messagingSenderId: "746283097286",
+//    appId: "1:746283097286:web:f3b53c0167df1e9c2a4bc7"
+//  };
+//  // Initialize Firebase
+
+// Firebase.initializeApp(FirebaseConfig);
+// const firestore:Firebase.firestore();
